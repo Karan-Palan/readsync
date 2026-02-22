f1fedbf778f68e6954c0e2adc8083175f0384f34
karan-palan <karanpalan007@gmail.com>
2026-02-22 21:04:59 +0530

    chore: remaining copy
    Signed-off-by: karan-palan <karanpalan007@gmail.com>

--- apps/web/public/foliate-js/index.js
+++ apps/web/public/foliate-js/index.js
@@ -1,38 +1,40 @@
 // Main entry point for foliate-js
 // Core classes
+export { View, ResponseError, NotFoundError, UnsupportedTypeError, makeBook } from './view.js'
+export { EPUB } from './epub.js'
+export { MOBI, isMOBI } from './mobi.js'
+export { makePDF, initializePDF, pdfjsLib } from './pdf.js'
+export { makeFB2 } from './fb2.js'
+export { makeComicBook } from './comic-book.js'
 
-export { makeComicBook } from "./comic-book.js";
-// Dictionary support
-export { DictdDict, StarDict } from "./dict.js";
-export { EPUB } from "./epub.js";
-// EPUB CFI utilities
-export * as CFI from "./epubcfi.js";
-export { makeFB2 } from "./fb2.js";
-export { FixedLayout } from "./fixed-layout.js";
-export { FootnoteHandler } from "./footnotes.js";
-export { isMOBI, MOBI } from "./mobi.js";
-// OPDS support
-export * as OPDS from "./opds.js";
-export { Overlayer } from "./overlayer.js";
 // UI Components
-export { Paginator } from "./paginator.js";
-export { initializePDF, makePDF, pdfjsLib } from "./pdf.js";
+export { Paginator } from './paginator.js'
+export { FixedLayout } from './fixed-layout.js'
+export { Overlayer } from './overlayer.js'
+export { FootnoteHandler } from './footnotes.js'
+export { TTS } from './tts.js'
+
 // Progress and Navigation
-export { SectionProgress, TOCProgress } from "./progress.js";
+export { TOCProgress, SectionProgress } from './progress.js'
+
 // Search functionality
-export { search, searchMatcher } from "./search.js";
+export { search, searchMatcher } from './search.js'
+
 // Text processing
-export { textWalker } from "./text-walker.js";
-export { TTS } from "./tts.js";
-// UI utilities
-export { createMenu } from "./ui/menu.js";
-export { createTOCView } from "./ui/tree.js";
+export { textWalker } from './text-walker.js'
+
+// Dictionary support
+export { DictdDict, StarDict } from './dict.js'
+
+// EPUB CFI utilities
+export * as CFI from './epubcfi.js'
+
+// OPDS support
+export * as OPDS from './opds.js'
+
 // URI Template utilities
-export * as URITemplate from "./uri-template.js";
-export {
-	makeBook,
-	NotFoundError,
-	ResponseError,
-	UnsupportedTypeError,
-	View,
-} from "./view.js";
+export * as URITemplate from './uri-template.js'
+
+// UI utilities
+export { createMenu } from './ui/menu.js'
+export { createTOCView } from './ui/tree.js' 
